4b3f2e60b9c0ae2bc4d18e288a91113d67732fa2
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2024-09-15 01:30:08 +0700

    Episode 13 - Studi Kasus bag.1

--- Episode 13 - Studi Kasus/portfolio-tailwind-css/tailwind.config.js
+++ Episode 13 - Studi Kasus/portfolio-tailwind-css/tailwind.config.js
@@ -2,11 +2,18 @@
 module.exports = {
   content: ["./resources/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
         primary: "#F7A92D",
         secondary: "#182B1B",
         dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
       },
     },
   },
