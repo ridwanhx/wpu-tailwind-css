8a306d046ee24742a2d16b476ebf6dc9dd380b41
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2024-09-17 00:37:02 +0700

    Episode 13 : Studi Kasus Website Portfolio menggunakan Tailwind CSS - styling navbar

--- Episode 13 - Studi Kasus/portfolio-tailwind-css/resources/js/script.js
+++ Episode 13 - Studi Kasus/portfolio-tailwind-css/resources/js/script.js
@@ -1,24 +1,34 @@
 // hamburger
 // tangkap elemen hamburgernya terlebih dahulu
-const hamburger = document.querySelector('#hamburger');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 // berikan event terhadap elemen, ketika elemen di klik
-hamburger.addEventListener('click', function () {
-    // add : menambahkan kelas pada elemen
-    // remove : menghilangkan kelas pada elemen
-    // toggle : melakukan keduanya (menambahkan / menghilangkan)
-    hamburger.classList.toggle('hamburger-active');
+hamburger.addEventListener("click", function () {
+  // add : menambahkan kelas pada elemen
+  // remove : menghilangkan kelas pada elemen
+  // toggle : melakukan keduanya (menambahkan / menghilangkan)
+  hamburger.classList.toggle("hamburger-active");
+  // muncul / hilangkan kelas hidden ketika fungsi ini dijalankan
+  navMenu.classList.toggle("hidden");
 });
 
 // Navbar Fixed
+// jalankan fungsi berikut hanya ketika window dalam keadaan di scroll
 window.onscroll = function () {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+  // panggil elemen header
+  const header = document.querySelector("header");
+  // ambil posisi header terhadap ujung atas dari halamannya / jarak dari posisi header terhadap top nya
+  const fixedNav = header.offsetTop;
 
-    if (window.scrollY > fixedNav) {
-        header.classList.add('navbar-fixed');
-        header.classList.remove('absolute')
-    } else {
-        header.classList.remove('navbar-fixed');
-        header.classList.add('absolute')
-    }
+  // jika scroll yang sedang saya lakukan lebih besar dari fixed-nav(secara default fixedNav akan bernilai 0) / jika jarak antara window dengan fixedNav nya ada selisih
+  if (window.scrollY > fixedNav) {
+    // tambahkan kelas navbar-fixed kedalam elemen
+    header.classList.add("navbar-fixed");
+    // hilangkan kelas absolute kedalam elemen
+    header.classList.remove("absolute");
+  } else {
+    // lakukan kebalikannya untuk kondisi else
+    header.classList.remove("navbar-fixed");
+    header.classList.add("absolute");
+  }
 };
